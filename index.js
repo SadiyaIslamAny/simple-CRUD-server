3595b8387ccee621f105a0da3bd7e04eeded9c06
SADIA <you@example.com>
2026-06-07 21:11:29 +0600

    added users route to users data

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
+
+
+
+const uri = `mongodb+srv://simpleCRUDUser:gJeBvty3tMUu64QE@cluster0.epzzkkw.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+const run = async() =>{
+    try{
+       await client.connect();
+
+       const db = client.db("simpleCRUD");
+       const userCollection = db.collection("users")
+
+       app.get('/users', async(req, res) =>{
+              const cursor = userCollection.find();
+              const result = await cursor.toArray();
+              res.send(result);
+       })
+       await client.db("admin").command({ ping: 1 });
+       console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }
+
+    finally{
+            //    await client.close();
+    }
+};
+
+run().catch(console.dir);
+
+// gJeBvty3tMUu64QE 
+// simpleCRUDUser
+// mongodb+srv://simpleCRUDUser:gJeBvty3tMUu64QE@cluster0.epzzkkw.mongodb.net/?appName=Cluster0
+// mongodb+srv://<db_username>:<db_password>@cluster0.epzzkkw.mongodb.net/?appName=Cluster0
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res)=>{
+    res.send("hello any");
+})
+
+
+app.listen(port, () =>{
+      console.log(`Example app listening on port ${port}`)
+})
