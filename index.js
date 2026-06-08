97d7aafe00cfefed1cd45ff78d5b98590e6067c2
SADIA <you@example.com>
2026-06-08 22:34:58 +0600

    Create Express DELETE API to remove user from MongoDB

--- index.js
+++ index.js
@@ -5,46 +5,58 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 
-
 const uri = `mongodb+srv://simpleCRUDUser:gJeBvty3tMUu64QE@cluster0.epzzkkw.mongodb.net/?appName=Cluster0`;
 
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
-const run = async() =>{
-    try{
-       await client.connect();
+const run = async () => {
+    try {
+        await client.connect();
 
-       const db = client.db("simpleCRUD");
-       const userCollection = db.collection("users")
+        const db = client.db("simpleCRUD");
+        const userCollection = db.collection("users")
 
-       app.get('/users', async(req, res) =>{
-              const cursor = userCollection.find();
-              const result = await cursor.toArray();
-              res.send(result);
-       })
+        app.get('/users', async (req, res) => {
+            const cursor = userCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
 
-       app.get('/users/:id', async(req,res) =>{
-        const id = req.params.id;
-        const query = {
-            _id: new ObjectId(id)
-        }
-        const user = await userCollection.findOne(query)
-        console.log(id)
-        res.send(user)
-       })
+        app.get('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {
+                _id: new ObjectId(id)
+            }
+            const user = await userCollection.findOne(query)
+            console.log(id)
+            res.send(user)
+        })
 
-       await client.db("admin").command({ ping: 1 });
-       console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id)
+            const query = {
+                _id: new ObjectId(id)
+            };
+
+            const result = await userCollection.deleteOne(query);
+
+            res.send(result);
+        });
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
     }
 
-    finally{
-            //    await client.close();
+    finally {
+        //    await client.close();
     }
 };
 
@@ -58,11 +70,11 @@ run().catch(console.dir);
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send("hello any");
 })
 
 
-app.listen(port, () =>{
-      console.log(`Example app listening on port ${port}`)
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
 })
